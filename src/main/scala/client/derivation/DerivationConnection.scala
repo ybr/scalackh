9e97de8f2c7914535528f8303ca64409df06efa4
Yohann Brédoux <yohann.bredoux@gmail.com>
2019-04-04 23:22:27 +0200

    refactor: add underlying on DerivationConnection

--- src/main/scala/client/derivation/DerivationConnection.scala
+++ src/main/scala/client/derivation/DerivationConnection.scala
@@ -1,11 +1,13 @@
 package scalackh.client.derivation
 
-import scalackh.protocol._
+import scalackh.protocol.Block
+import scalackh.client.Connection
 import scalackh.transpose.ColumnTransposer
 
 import scala.util.Try
 
 trait DerivationConnection {
+  def underlying(): Connection
   def query[A](sql: String, externalTables: Iterator[Block] = Iterator.empty)(implicit colA: ColumnTransposer[A]): Iterator[Try[List[A]]]
   def insert[A](sql: String, values: Iterator[A])(implicit colA: ColumnTransposer[A]): Unit
   def disconnect(): Unit
