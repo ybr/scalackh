242d1e75332583dae662e13aa547d62112a8481f
Yohann Brédoux <yohann.bredoux@gmail.com>
2019-04-16 22:19:36 +0200

    dead code

--- src/main/scala/client/derivation/DerivationUtils.scala
+++ src/main/scala/client/derivation/DerivationUtils.scala
@@ -10,10 +10,4 @@ object DerivationUtils {
     val columns: List[ColumnData] = colA.toColumnsData(valuesForBlock)
     Block(None, BlockInfo.empty, columns.length, valuesForBlock.length, columns.map(Column("", _)))
   }
-
-  def anonymeBlocks[A](as: Iterator[A])(implicit colA: ColumnTransposer[A]): Iterator[AnonymBlock] = as.grouped(ProtocolSteps.MAX_ROWS_IN_BLOCK).map { group =>
-    val valuesForBlock = group.toList
-    val columnsData: List[ColumnData] = colA.toColumnsData(valuesForBlock)
-    AnonymBlock(columnsData.length, valuesForBlock.length, columnsData)
-  }
 }
