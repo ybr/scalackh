a752d6602c4cc501b1c6ac091e31a27e4721951e
Yohann Brédoux <yohann.bredoux@gmail.com>
2020-10-31 10:55:56 +0100

    better exception messages

--- src/main/scala/protocol/exceptions.scala
+++ src/main/scala/protocol/exceptions.scala
@@ -2,5 +2,6 @@ package scalackh.protocol
 
 class ClickhouseClientException(message: String) extends RuntimeException(message)
 class ClickhouseServerException(cause: ServerException) extends RuntimeException {
-  override def toString(): String = cause.toString()
+  override def toString(): String = s"ClickhouseServerException(${cause.toString()})"
+  override def getMessage(): String = cause.message
 }
